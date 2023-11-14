a64aff0d22f5711606278cdd0f9a2c028c21533d
Joao Astolfi <jpastolfi@gmail.com>
2023-11-14 00:36:18 +0000

    README update and translations

--- pages/index.tsx
+++ pages/index.tsx
@@ -165,7 +165,7 @@ export default function HomePage() {
                         });
                     }}
                   >
-                    Apagar
+                    Delete
                   </button>
                 </td>
               </tr>
